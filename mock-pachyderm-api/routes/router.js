47c0d2109ce7ea92444ef2b7a7af500e5ae8ee80
Minoru Nakano <minoru.nakano@gmail.com>
2020-01-30 10:55:09 -0500

    build-test-20200130

--- mock-pachyderm-api/routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const process = require('./process');
-
-router.get('/pipeline/start', process.startPipeline);
-router.get('/complete', process.completeProcess);
-
-module.exports = router;
