5a513d28b0f31505187b3e2bc4c212332974d0ad
Minoru Nakano <minoru.nakano@gmail.com>
2020-02-28 17:23:21 -0500

    Started filling in the documentation page.

--- client/src/components/Documentation/DocFunctionality/Request.js
+++ client/src/components/Documentation/DocFunctionality/Request.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {ScrollPanel} from 'primereact/scrollpanel';
 
 const Request = () => {
     return(
         <div className='documentation'>
-            <h1>Request</h1>  
+            <h1>Request</h1> 
+            <ScrollPanel className='documentation-scroll'>
+                <div>
+                    <h3>How to request a PSet</h3>
+                    <p>
+                        To request a PSet, turn on the Request mode on <a href='/PSetSearch'>Search and Request</a> view with the toggle button on top of the PSet Parameters pane. In the requets mode, two text fields to enter the PSet name and email address to receive pipeline completion notification will be displayed.<br />
+                        Once all the information is filled out, including the PSet name and email address, the Submit Request button becomes active.<br />
+                        PSet request is submitted upon clicking the Submit button.
+                    </p> 
+                    <div className='img-full'>
+                        <img src={process.env.PUBLIC_URL + "/images/documentation/request.png"} />
+                    </div>  
+                    <h3>How to check status of your request</h3>
+                    <p>
+                        Status of your pipeline request can be viewed at <a href='/Dashboard'>Request Status</a> view.<br /> 
+                        Status of your request can either be "pending" or "in-process". Pending requests are the ones that have been successfully submitted, and will be processed once the Pachyderm cluster is online. In-process requests have been successfully submitted, and the pipeline execution has been started.
+                    </p> 
+                    <div className='img-full'>
+                        <img src={process.env.PUBLIC_URL + "/images/documentation/request_status.png"} />
+                    </div> 
+                </div>
+            </ScrollPanel>   
         </div>    
     );
 }
--- client/src/components/Documentation/DocFunctionality/Search.js
+++ client/src/components/Documentation/DocFunctionality/Search.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {ScrollPanel} from 'primereact/scrollpanel';
 
 const Search = () => {
     return(
         <div className='documentation'>
             <h1>Search</h1>
+            <ScrollPanel className='documentation-scroll'>
+                <div>
+                    <h3>How to search existing PSets</h3>
+                    <p>
+                        Existing PSets can be searched on <a href='/PSetSearch'>Search and Request</a> page.<br />
+                        User can select different pipeline parameters uder PSet Parameters pane. Each selection of parameters triggers a database search. The search result is displayed on the data table as shown in the figure below.
+                    </p> 
+                    <div className='img-full'>
+                        <img src={process.env.PUBLIC_URL + "/images/documentation/search.png"} />
+                    </div>  
+                    <h3>How to view metadata of a PSet</h3>
+                    <p>
+                        Metadata of a PSet can be viewed by clicking a dataset name on the search table.<br />
+                        Explore PSet view is displayed as in the figure below where user can view publications associated with the dataset, and command lines used in the pipeline to generate the PSet.
+                    </p> 
+                    <div className='img-full'>
+                        <img src={process.env.PUBLIC_URL + "/images/documentation/explore_pset.png"} />
+                    </div> 
+                </div>
+            </ScrollPanel>        
         </div>    
     );
 }
