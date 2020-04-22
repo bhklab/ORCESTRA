3e0b058a5c6418d53aaadda03b86df7099384d96
Minoru Nakano <minoru.nakano@gmail.com>
2020-04-22 17:49:43 -0400

    Changed the dataset visualization to "UpSet" style plot.

--- client/src/components/Stats/DatasetChart.js
+++ client/src/components/Stats/DatasetChart.js
@@ -2,18 +2,11 @@ import React, {useEffect, useState} from 'react';
 import Plotly from "plotly.js-basic-dist";
 import createPlotlyComponent from "react-plotly.js/factory";
 import {Checkbox} from 'primereact/checkbox';
+import {RadioButton} from 'primereact/radiobutton';
 const Plot = createPlotlyComponent(Plotly);
 
 
 const DatasetChart = props => {
-
-    const metricValues = {
-        numCellLines: '<b>Number of <br />cell lines</b>', 
-        numDrugs: '<b>Number of <br />drugs</b>',
-        numDrugSensitivityExp: '<b>Number of <br />drug sensitivity <br />experiments</b>', 
-        numGenes: '<b>Number of genes</b>'
-    }
-
     const defColors = [
         '#1f77b4',  // muted blue
         '#ff7f0e',  // safety orange
@@ -22,13 +15,24 @@ const DatasetChart = props => {
         '#9467bd',  // muted purple
         '#8c564b',  // chestnut brown
         '#e377c2',  // raspberry yogurt pink
-        '#7f7f7f',  // middle gray
+        '#6fbd22',  // 
         '#bcbd22',  // curry yellow-green
         '#17becf'   // blue-teal
-    ];
+    ]
+
+    const metricSet = [
+        {name: 'Numer of Cell Lines', value: 'numCellLines'},
+        {name: 'Numer of Drugs', value: 'numDrugs'},
+        {name: 'Numer of Drug Sensitivity Experiments', value: 'numDrugSensitivityExp'},
+        {name: 'Numer of Genes', value: 'numGenes'}
+    ]
 
     const [data, setData] = useState([])
-    const [parameters, setParameters] = useState({})
+    const [barData, setBarData] = useState([])
+    const [parameters, setParameters] = useState({
+        datasets: {},
+        metricName: ''
+    })
     const [isReady, setIsReady] = useState(false)
 
     useEffect(() => {
@@ -44,65 +48,69 @@ const DatasetChart = props => {
                 colIndex++
             }
         }
-        setParameters({...datasets, 
-            numCellLines: {name: 'Numer of Cell Lines', checked: true, isMetric: true},
-            numDrugs: {name: 'Numer of Drugs', checked: true, isMetric: true},
-            numDrugSensitivityExp: {name: 'Numer of Drug Sensitivity Experiments', checked: false, isMetric: true},
-            numGenes: {name: 'Numer of Genes', checked: false, isMetric: true}
+        setParameters({
+            datasets: {...datasets},
+            metricName: 'numCellLines' 
         })
         setIsReady(true)
     }, [])
 
     useEffect(() => {
         if(isReady){
-            let array = []
+            let datasets = []
             for(let i = 0; i < props.chartData.length; i++){
                 for(let j = 0; j < props.chartData[i].versions.length; j++){
-                    let metric = props.chartData[i].versions[j].metric
-                    if(parameters[props.chartData[i].name + '_' + j].checked && metric.numCellLines){
-                        let selectedMetrics = []
-                        let metricNames = []
-                        if(parameters.numCellLines.checked) {
-                            selectedMetrics.push(metric.numCellLines)
-                            metricNames.push(metricValues.numCellLines)
-                        }
-                        if(parameters.numDrugs.checked) {
-                            selectedMetrics.push(metric.numDrugs)
-                            metricNames.push(metricValues.numDrugs)
-                        }
-                        if(parameters.numDrugSensitivityExp.checked) {
-                            selectedMetrics.push(metric.numDrugSensitivityExp)
-                            metricNames.push(metricValues.numDrugSensitivityExp)
-                        }
-                        if(parameters.numGenes.checked) {
-                            selectedMetrics.push(metric.numGenes)
-                            metricNames.push(metricValues.numGenes)
-                        }
-                        array.push({
-                            y: metricNames,
-                            x: selectedMetrics,
-                            name: parameters[props.chartData[i].name + '_' + j].name,
-                            mode: 'markers',
-                            type: 'scatter',
-                            marker: {
-                                color: parameters[props.chartData[i].name + '_' + j].color,
-                                size: []
-                            }
+                    if(parameters.datasets[props.chartData[i].name + '_' + j].checked){
+                        datasets.push({
+                            name: parameters.datasets[props.chartData[i].name + '_' + j].name,
+                            value: props.chartData[i].versions[j].metric[parameters.metricName],
+                            color: parameters.datasets[props.chartData[i].name + '_' + j].color
                         })
                     }
                 }
             }
-            for(let i = 0; i < array.length; i++){
-                for(let j = 0; j < array[i].x.length; j++){
-                    let metricVals = array.map(item => {
-                        return (item.x[j])
-                    })
-                    metricVals.sort(function(a, b){return a - b})
-                    let index = metricVals.indexOf(array[i].x[j]) + 1
-                    array[i].marker.size.push((1/metricVals.length) * index * 50)
-                }
+
+            datasets.sort((a, b) => (a.value > b.value) ? 1: -1)
+            
+            setData([{
+                x: datasets.map((item) => {return item.value}),
+                y: datasets.map((item) => {return item.name}),
+                text: datasets.map((item) => {return (item.name + ': ' + item.value)}),
+                mode: 'markers',
+                type: 'scatter',
+                marker: {
+                    size: 12,
+                    color: datasets.map((item) => {return item.color})
+                },
+                hoverinfo: 'text'
+            }])
+
+            let values = datasets.map((item) => {return item.value})
+            let duplicates = [...new Set(values.filter((item, index) => values.indexOf(item) != index))]
+
+            console.log(duplicates)
+
+            let bars = []
+            for(let i = 0; i < datasets.length; i++){
+                bars.push({
+                    x: [datasets[i].value, datasets[i].value],
+                    y: [0, datasets[i].value],
+                    name: datasets[i].name,
+                    mode: 'lines+markers',
+                    type: 'scatter',
+                    line: {
+                        color: duplicates.indexOf(datasets[i].value) < 0 ? datasets[i].color : '#777777',
+                        width: 12
+                    },
+                    marker: {
+                        color: duplicates.indexOf(datasets[i].value) < 0 ? datasets[i].color : '#777777',
+                        symbol: 'line-ew',
+                        size: 12
+                    },
+                })
             }
-            setData(array)
+
+            setBarData(bars)
         }
     }, [parameters])
     
@@ -111,63 +119,88 @@ const DatasetChart = props => {
             <h4>Select Dataset Metrics to View</h4>
             <div className='stats-dataset-metric-control'>
                 {
-                    Object.keys(parameters).map((key) => {
-                        if(parameters[key].isMetric){
-                            return(
-                                <div className='stats-chart-control-checkbox' key={key}>
-                                    <Checkbox inputId={key} value={key} checked={parameters[key].checked}
-                                        onChange={e => {
-                                            let params = {...parameters}
-                                            params[key].checked = e.checked
-                                            setParameters(params)
-                                        }
-                                    }></Checkbox>
-                                    <label htmlFor={key}>{parameters[key].name}</label>
-                                </div>
-                            )
-                        }
+                    metricSet.map((metric) => {
+                        return(
+                            <div className='stats-chart-control-checkbox' key={metric.value}>
+                                <RadioButton inputId={metric.name} value={metric.value} checked={parameters.metricName === metric.value}
+                                    onChange={e => {
+                                        let params = {...parameters}
+                                        params.metricName = e.value
+                                        setParameters(params)
+                                    }
+                                }></RadioButton>
+                                <label htmlFor={metric.value}>{metric.name}</label>
+                            </div>
+                        )
                     })
                 }
             </div>    
             <div className='stats-dataset-chart'>
                 <div className='stats-dataset-plot'>
                     <Plot
+                        data= {barData}
+                        layout={ {
+                            autosize: true,
+                            xaxis: {
+                                showgrid: false,
+                                zeroline: false,
+                                showticklabels: false,
+                                autorange: 'reversed',
+                            },
+                            yaxis: {
+                                tickfont: {
+                                    size: 12
+                                },
+                                showgrid: true,
+                                showticklabels: true
+                            },
+                            margin: {t: 10, b: 0, l: 100, r: 10},
+                            showlegend: false
+                        } }
+                        style = {{width: "100%", height: '250px', marginLeft: 'auto', marginRight: 'auto'}}
+                        useResizeHandler = {true}
+                        config = {{displayModeBar: false}}
+                    />
+                    <Plot
                         data= {data}
                         layout={ {
                             autosize: true,
                             xaxis: {
-                                showgrid: true
+                                showgrid: false,
+                                zeroline: false,
+                                autorange: 'reversed',
+                                showticklabels: false
                             },
                             yaxis: {
-                                automargin: true,
+                                showgrid: true,
                                 tickfont: {
-                                    size: 14
+                                    size: 12
                                 },
-                                showgrid: false
-                            }
+                                showticklabels: true
+                            },
+                            margin: {t: 0, b: 10, l: 100, r: 10}
                         } }
-                        style = {{width: "100%"}}
+                        style = {{width: "100%", height: '200px'}}
                         useResizeHandler = {true}
+                        config = {{displayModeBar: false}}
                     />
                 </div>
                 <div className='stats-dataset-chart-control'>
                     <h4>Select Dataset(s) to View</h4>
                     {
-                        Object.keys(parameters).map((key) => {
-                            if(!parameters[key].isMetric){
-                                return(
-                                    <div className='stats-chart-control-checkbox' key={key}>
-                                        <Checkbox inputId={key} value={key} checked={parameters[key].checked}
-                                            onChange={e => {
-                                                let params = {...parameters}
-                                                params[key].checked = e.checked
-                                                setParameters(params)
-                                            }
-                                        }></Checkbox>
-                                        <label htmlFor={key}>{parameters[key].name}</label>
-                                    </div>
-                                )
-                            }
+                        Object.keys(parameters.datasets).map((key) => {
+                            return(
+                                <div className='stats-chart-control-checkbox' key={key}>
+                                    <Checkbox inputId={key} value={key} checked={parameters.datasets[key].checked}
+                                        onChange={e => {
+                                            let params = {...parameters}
+                                            params.datasets[key].checked = e.checked
+                                            setParameters(params)
+                                        }
+                                    }></Checkbox>
+                                    <label htmlFor={key}>{parameters.datasets[key].name}</label>
+                                </div>
+                            )
                         })
                     }
                 </div>    
