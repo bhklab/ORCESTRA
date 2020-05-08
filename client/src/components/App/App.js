a80cc067c3977c311e108fd59e02a28b74586b2c
Minoru Nakano <minoru.nakano@gmail.com>
2020-05-08 12:46:45 -0400

    Added Google Analytics

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import ReactGA from 'react-ga';
 import './App.css';
 import './PrimeStyle.css';
 import Router from '../Routing/Router';
 
-class App extends React.Component {
-	render(){
-		return (
-			<div className="App">
-				<Router />
-			</div>
-		);
-	}
+const App = () => {
+	
+	// Google Analytics set up
+	useEffect(() => {
+		ReactGA.initialize('UA-102362625-2')
+		ReactGA.pageview(window.location.pathname + window.location.search)
+	}, [])
+	
+	return (
+		<div className="App">
+			<Router />
+		</div>
+	)
 }
 
 export default App;
