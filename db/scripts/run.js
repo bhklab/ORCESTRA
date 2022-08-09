d4a42abd804290061b74e12d50d92355c9ad220b
Minoru Nakano <minoru.nakano@gmail.com>
2022-08-09 14:03:14 -0400

    updated script

--- db/scripts/run.js
+++ db/scripts/run.js
@@ -17,38 +17,42 @@ const PachydermPipeline = require('../models/pachyderm-pipeline');
         await mongoose.connect(process.env.Prod, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('connection open');
 
-        // let datasets = await Dataset.find({datasetType: 'clinical_icb'});
-        // let objInfo = fs.readFileSync('../data/new_obj.json');
-        // objInfo = JSON.parse(objInfo);
-        // let dataObjects = [];
+        let datasets = await Dataset.find({datasetType: 'clinical_icb'});
+        let objInfo = fs.readFileSync('../data/new_obj.json');
+        objInfo = JSON.parse(objInfo);
+        let dataObjects = [];
         
-        // for(const dataset of datasets){
-        //     let obj = objInfo.find(item => item.pipeline.name === dataset.name);
-        //     const info = {
-        //         date: {created: obj.process_end_date},
-        //         status: "complete",
-        //         private: false,
-        //         canonical: true,
-        //         numDownload: 0,
-        //         createdBy: "BHK Lab",
-        //         other: {
-        //             pipeline: {url: obj.pipeline.git_url, commit_id: obj.commit_id},
-        //             additionalRepo: obj.additional_repo
-        //         }
-        //     }
-        //     dataObjects.push({
-        //         info: info,
-        //         datasetType: 'clinical_icb',
-        //         name: dataset.name,
-        //         dataset: dataset._id,
-        //         repositories: [
-        //             {version: '1.0', doi: obj.doi, downloadLink: obj.download_link}
-        //         ],
-        //         availableDatatypes: dataset.availableData.map(item => ({name: item.name, genomeType: item.datatype}))
-        //     })
-        // }
+        for(const dataset of datasets){
+            let obj = objInfo.find(item => item.pipeline.name === dataset.name);
+            if(obj){
+                const info = {
+                    date: {created: obj.process_end_date},
+                    status: "complete",
+                    private: true,
+                    canonical: true,
+                    numDownload: 0,
+                    createdBy: "BHK Lab",
+                    other: {
+                        pipeline: {url: obj.pipeline.git_url, commit_id: ""},
+                        additionalRepo: obj.additional_repo
+                    }
+                }
+                dataObjects.push({
+                    info: info,
+                    datasetType: 'clinical_icb',
+                    name: dataset.name,
+                    dataset: dataset._id,
+                    repositories: [
+                        {version: '1.0', doi: obj.doi, downloadLink: obj.download_link}
+                    ],
+                    availableDatatypes: dataset.availableData.map(item => ({name: item.name, genomeType: item.datatype}))
+                })
+            }
+        }
 
-        // await ObjSchema.BaseDataObject.insertMany(dataObjects)
+        // console.log(dataObjects)
+
+        await ObjSchema.BaseDataObject.insertMany(dataObjects)
 
         // await ObjSchema.BaseDataObject.deleteMany({datasetType: 'clinical_icb'})
     }catch(err){
