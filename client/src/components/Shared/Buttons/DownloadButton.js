29884e6889174f725438e8e4902934b459af56e4
Matthew Boccalon <matthew.boccalon@live.ca>
2024-06-13 15:07:02 -0400

    Added custom stylings to default PrimeReact button, changed singleDataset buttons back to PrimeReact button, reconfigured hover state for Mainstyle button, added auto formatting on save

--- client/src/components/Shared/Buttons/DownloadButton.js
+++ client/src/components/Shared/Buttons/DownloadButton.js
@@ -2,39 +2,38 @@ import React from 'react';
 import axios from 'axios';
 import { Button } from 'primereact/button';
 
-const DownloadButton = (props) => {
-
+const DownloadButton = props => {
     const { className, datasetType, doi, downloadLink, mode, disabled, label, tooltip } = props;
 
-    const download = async (event) => {
+    const download = async event => {
         event.preventDefault();
 
         const anchor = document.createElement('a');
         anchor.setAttribute('download', null);
         anchor.style.display = 'none';
-        anchor.setAttribute('href', downloadLink)
+        anchor.setAttribute('href', downloadLink);
         document.body.appendChild(anchor);
         anchor.click();
         document.body.removeChild(anchor);
 
-        if(mode === 'dataset'){
+        if (mode === 'dataset') {
             await axios.post('/api/data-object/download', {
                 datasetType: datasetType,
                 datasetDOI: doi
             });
         }
-    }
+    };
 
-    return(
-        <Button 
-            className={className} 
-            label={label} 
-            icon='pi pi-download'
-            disabled={disabled} 
+    return (
+        <Button
+            className={className}
+            label={label}
+            icon="pi pi-download"
+            disabled={disabled}
             onClick={download}
             tooltip={tooltip}
         />
     );
-}
+};
 
 export default DownloadButton;
--- client/src/hooks/useSingleDataset.js
+++ client/src/hooks/useSingleDataset.js
@@ -12,7 +12,6 @@ import DownloadButton from '../components/Shared/Buttons/DownloadButton';
 import CustomMessages from '../components/Shared/CustomMessages';
 import CustomSelect from '../components/Shared/CustomSelect';
 import { dataTypes } from '../components/Shared/Enums';
-import * as Mainstyle from '../components/Main/MainStyle'
 
 const Header = styled.div`
     width: 100%;
@@ -170,32 +169,30 @@ const useSingleDataset = (datasetType, doi) => {
                             }))}
                             onChange={(e) => {setSelectedObject(e.value)}}
                         />
-                        <Mainstyle.Button 
-                        className='left' 
-                        disabled={typeof selectedObject === 'undefined'} 
-                        datasetType={datasetType} 
-                        doi={dataset.data.doi}
-                        downloadLink={typeof selectedObject !== 'undefined' ? selectedObject.value.link : ''}
-                        mode='dataset'
-                        tooltip={`Download ${dataset.data.name}(${typeof selectedObject !== 'undefined' ? selectedObject.value.name : ''}) as an R object`}
-                    > 
-                    Download Dataset
-                    </Mainstyle.Button>
+                        <DownloadButton 
+                            className='left' 
+                            disabled={typeof selectedObject === 'undefined'} 
+                            datasetType={datasetType} 
+                            doi={dataset.data.doi}
+                            downloadLink={typeof selectedObject !== 'undefined' ? selectedObject.value.link : ''}
+                            mode='dataset'
+                            label='Download Dataset'
+                            tooltip={`Download ${dataset.data.name}(${typeof selectedObject !== 'undefined' ? selectedObject.value.name : ''}) as an R object`}
+                        />
                     </React.Fragment>
                 );
             }
             return(
-                <Mainstyle.Button
+                <DownloadButton 
                     className='left' 
                     disabled={false} 
                     datasetType={datasetType} 
                     doi={dataset.data.doi}
                     downloadLink={dataset.data.downloadLink}
                     mode='dataset'
+                    label='Download Dataset'
                     tooltip={`Download ${dataset.data.name} as an R object`}
-                >
-                    Download Dataset
-                </Mainstyle.Button>
+                />
             );
         // }
         // return '';
@@ -212,17 +209,16 @@ const useSingleDataset = (datasetType, doi) => {
                 }
                 {
                     publicView && dataset.data.bioComputeObject &&
-                    <Mainstyle.Button 
+                    <DownloadButton 
                         className='left' 
                         disabled={false} 
                         datasetType={datasetType} 
                         doi={dataset.data.bioComputeObject.doi}
                         downloadLink={dataset.data.bioComputeObject.downloadLink}
                         mode='bioCompute'
+                        label='Download BioCompute Object'
                         tooltip={`Download the BioCompute object of the pipleine used to create ${dataset.data.name}`}
-                    >
-                        Download BioCompute Object
-                    </Mainstyle.Button>
+                    />
                 }
                 { 
                     ownerView && 
