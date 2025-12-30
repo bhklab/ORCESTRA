dd4d443ec219e886b1d99b3ec9228b3f1a02bac4
mattbocc <matthew.boccalon@live.ca>
2025-12-30 14:51:59 -0500

    feat: added testable datable and multiselectors for dataset list page.

---
+++ client/src/components/SearchRequest/All/Dropdowns/DatasetSelect.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { MultiSelect } from 'primereact/multiselect';
+
+export const DatasetSelect = ({ datasets }) => {
+    const [selectedCities, setSelectedCities] = useState(null);
+    const cities = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
+    return (
+        <div className="card flex justify-content-center">
+            <MultiSelect
+                value={selectedCities}
+                onChange={e => setSelectedCities(e.value)}
+                options={cities}
+                optionLabel="name"
+                filterDelay={400}
+                placeholder="Select Cities"
+                className="w-full md:w-20rem"
+                maxSelectedLabels={5}
+                filter
+            />
+        </div>
+    );
+};
--- client/src/components/SearchRequest/All/SetSearch.js
+++ client/src/components/SearchRequest/All/SetSearch.js
@@ -1,115 +1,127 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { Messages } from 'primereact/messages';
-import { MultiSelect } from 'primereact/multiselect';
 import { AuthContext } from '../../../hooks/Context';
-import useDatasetSearch from '../../../hooks/useDatasetSearch';
 import SaveDatasetButton from '../../Shared/Buttons/SaveDatasetButton';
-import { dataTypes } from '../../Shared/Enums';
+// import { dataTypes } from '../../Shared/Enums';
+import { dataTypes } from './dictionaries/dataTypes';
+import { DatasetSelect } from './Dropdowns/DatasetSelect';
+
+// PrimeReact Imports
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 const SetSearch = () => {
     const auth = useContext(AuthContext);
-    const { searchAll, search } = useDatasetSearch(dataTypes.radiomics);
+    const { datatype } = useParams();
 
     const [datasets, setDatasets] = useState([]);
     const [selectedDatasets, setSelectedDatasets] = useState([]);
 
-    const { datatype } = useParams();
-
-    const [datasetSelect, setDatasetSelect] = useState({ selected: [], options: [], hidden: false });
-    const [filterReady, setFilterReady] = useState(false);
-
-    const showMessage = (status, data) => {
-        let severity = status ? 'success' : 'error';
-        SetSearch.messages.show({
-            severity: severity,
-            summary: data.summary,
-            detail: data.message,
-            sticky: true
-        });
-        setSelectedDatasets([]);
-    };
+    const products = [
+        {
+            id: '1000',
+            code: 'f230fh0g3',
+            name: 'Bamboo Watch',
+            description: 'Product Description',
+            image: 'bamboo-watch.jpg',
+            price: 65,
+            category: 'Accessories',
+            quantity: 24,
+            inventoryStatus: 'INSTOCK',
+            rating: 5
+        },
+        {
+            id: '1001',
+            code: 'nvklal433',
+            name: 'Black Watch',
+            description: 'Product Description',
+            image: 'black-watch.jpg',
+            price: 72,
+            category: 'Accessories',
+            quantity: 61,
+            inventoryStatus: 'INSTOCK',
+            rating: 4
+        },
+        {
+            id: '1002',
+            code: 'zz21cz3c1',
+            name: 'Blue Band',
+            description: 'Product Description',
+            image: 'blue-band.jpg',
+            price: 79,
+            category: 'Fitness',
+            quantity: 2,
+            inventoryStatus: 'LOWSTOCK',
+            rating: 3
+        },
+        {
+            id: '1003',
+            code: '244wgerg2',
+            name: 'Blue T-Shirt',
+            description: 'Product Description',
+            image: 'blue-t-shirt.jpg',
+            price: 29,
+            category: 'Clothing',
+            quantity: 25,
+            inventoryStatus: 'INSTOCK',
+            rating: 5
+        },
+        {
+            id: '1004',
+            code: 'h456wer53',
+            name: 'Bracelet',
+            description: 'Product Description',
+            image: 'bracelet.jpg',
+            price: 15,
+            category: 'Accessories',
+            quantity: 73,
+            inventoryStatus: 'INSTOCK',
+            rating: 4
+        }
+    ];
 
     useEffect(() => {
         const initialize = async () => {
             const res = await axios.get(`/api/view/data-object-filter/${datatype}`);
-            setDatasetSelect({ ...datasetSelect, options: res.data.dataset });
-            setFilterReady(true);
         };
         initialize();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <div className="flex flex-col p-10 z-10 items-center min-h-screen">
-            <div className="flex flex-col gap-4 bg-white border-1 drop-shadow-sm rounded-lg p-6">
-                <h3 className="text-headingSm font-bold">Explore multimodal {datatype} datasets</h3>
-                {filterReady && (
-                    <div className="flex flex-row gap-6 justify-start">
-                        <div>
-                            <MultiSelect
-                                id="dataset"
-                                className="dropdown"
-                                optionLabel="label"
-                                value={datasetSelect.selected}
-                                options={datasetSelect.options}
-                                selected={datasetSelect.selected}
-                                onChange={e => {
-                                    setDatasetSelect({ ...datasetSelect, selected: e.value });
-                                }}
-                                filter={true}
-                                itemTemplate={
-                                    <div>
-                                        <span className="text-gray-100">{datasetSelect.options.label}</span>
-                                    </div>
-                                }
-                                selectedItemTemplate={<span>Select Dataset</span>}
+            <div className="flex flex-col gap-8 bg-white border-1 drop-shadow-sm rounded-lg p-10">
+                <div>
+                    <h2 className="text-headingXl font-bold text-lightBlue">
+                        Explore multimodal {dataTypes[datatype].heading}
+                    </h2>
+                    <div className="flex flex-col">
+                        {auth.user ? (
+                            <SaveDatasetButton
+                                selectedDatasets={selectedDatasets}
+                                disabled={selectedDatasets.length > 0 ? false : true}
+                                // onSaveComplete={showMessage}
                             />
-                        </div>
-
-                        <div>
-                            <MultiSelect
-                                id="dataset"
-                                className="dropdown"
-                                optionLabel="label"
-                                value={datasetSelect.selected}
-                                options={datasetSelect.options}
-                                onChange={e => {
-                                    setDatasetSelect({ ...datasetSelect, selected: e.value });
-                                }}
-                                filter={true}
-                                itemTemplate={
-                                    <div>
-                                        <span className="text-gray-100">{datasetSelect.options.label}</span>
-                                    </div>
-                                }
-                                selectedItemTemplate={<span>Select Datasets</span>}
-                            />
-                        </div>
-                        <div className="flex flex-row items-center gap-2">
-                            <span className="text-headingLg font-bold">{datasets.length}</span>
-                            <span className="">
-                                {searchAll
-                                    ? 'dataset(s) available.'
-                                    : `${datasets.length === 1 ? ' match' : ' matches'} found.`}
-                            </span>
-                        </div>
+                        ) : (
+                            <p className="text-bodyXs text-red-600">
+                                Login or register to save existing {datatype} to your profile.
+                            </p>
+                        )}
                     </div>
-                )}
-                <div className="flex flex-col">
-                    {auth.user ? (
-                        <SaveDatasetButton
-                            selectedDatasets={selectedDatasets}
-                            disabled={selectedDatasets.length > 0 ? false : true}
-                            onSaveComplete={showMessage}
-                        />
-                    ) : (
-                        <p className="text-bodyXs text-red-600">
-                            Login or register to save existing {datatype} to your profile.
-                        </p>
-                    )}
                 </div>
+                <DatasetSelect />
+                <DataTable
+                    value={products}
+                    tableStyle={{ minWidth: '50rem' }}
+                    size="small"
+                    showGridlines={true}
+                    stripedRows
+                >
+                    <Column field="code" header="Code"></Column>
+                    <Column field="name" header="Name"></Column>
+                    <Column field="category" header="Category"></Column>
+                    <Column field="quantity" header="Quantity"></Column>
+                </DataTable>
             </div>
         </div>
     );
