e1bf630898d34063deba17b939e463d3cfd36067
mattbocc <matthew.boccalon@live.ca>
2026-03-05 09:42:44 -0500

    feat: created datatype mapping table

--- client/src/components/SearchRequest/All/dictionaries/dataTypes.js
+++ client/src/components/SearchRequest/All/dictionaries/dataTypes.js
@@ -1,8 +1,34 @@
 export const dataTypes = {
     pset: {
-        heading: 'Pharmacogenomics Datasets'
+        heading: 'Pharmacogenomics',
+        set: 'Pset'
+    },
+    toxicoset: {
+        heading: 'Toxicogenomic',
+        set: 'ToxicoSet'
+    },
+    radioset: {
+        heading: 'Radiogenomic',
+        set: 'RadioSet'
+    },
+    xevaset: {
+        heading: 'Xenographic Pharmacogenomics',
+        set: 'XevaSet'
+    },
+    clinical_icb: {
+        heading: 'Immune Checkpoint Blockade',
+        set: ''
+    },
+    clinicalgenomics: {
+        heading: 'Clinical Genomics',
+        set: 'Clinical Genomics Set'
     },
     radiomicset: {
-        heading: 'Radiomic Datasets'
+        heading: 'Radiomic',
+        set: 'RadiomicSet'
+    },
+    annotations: {
+        heading: 'Annotation',
+        set: 'Annotation Set'
     }
 };
