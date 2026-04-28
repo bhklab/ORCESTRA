f2262030a4d96d4fd67105e574e076fc43a20734
mattbocc <matthew.boccalon@live.ca>
2026-04-28 14:18:43 -0400

    additional cleanup of datatype page to render data sources and counts correctly

--- client/src/components/Main/DataTypes.js
+++ client/src/components/Main/DataTypes.js
@@ -1,133 +1,70 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { PathContext } from '../../hooks/Context';
+import axios from 'axios';
 
 const DataTypes = () => {
     const path = useContext(PathContext);
     const navigate = useNavigate();
-
-    const data = [
-        {
-            name: 'Pharmacogenomics',
-            fieldTotals: ['Datasets: 33', 'Canonical: 16', 'TCL Cell Lines: 39'],
-            contains: ['rnaseq', 'cnv', 'mutation', 'methylation'],
-            description:
-                "Data focused on how an individual's genetic attributes affect the likely response to therapeutic drugs.",
-            img: 'pharmacogenomics.png',
-            path: 'pset'
-        },
-        {
-            name: 'Toxicogenomics',
-            fieldTotals: ['Datasets: 4', 'Canonical: 4'],
-            contains: ['microarray', 'drug response'],
-            description:
-                "Data focused on how the genome is involved in the body's response to environmental stressors and toxicants.",
-            img: 'toxicogenomics.png',
-            path: 'toxicoset'
-        },
-        {
-            name: 'Xenographic Pharmacogenomics',
-            fieldTotals: ['Datasets: 1', 'Canonical: 1'],
-            contains: ['rnaseq', 'cnv', 'mutation', 'methylation'],
-            description:
-                "Data focused on how an individual's genetic attributes affect the likely response to therapeutic drugs.",
-            img: 'xenographicpharmacogenomics.png',
-            path: 'xevaset'
-        },
-        {
-            name: 'Radiogenomics',
-            fieldTotals: ['Datasets: 1', 'Canonical: 1'],
-            contains: ['microarray', 'drug response'],
-            description:
-                "Data focused on how the genome is involved in the body's response to environmental stressors and toxicants.",
-            img: 'radiogenomics.png',
-            path: 'radioset'
-        },
-        {
-            name: 'Clinical Genomics',
-            fieldTotals: ['Datasets: 23', 'Canonical: 23'],
-            contains: ['rnaseq', 'microarray'],
-            description:
-                "Data focused with how an individual's genetic attributes affect the likely response to therapeutic drugs.",
-            img: 'clinicalgenomics.png',
-            path: 'clinicalgenomics'
-        },
-        {
-            name: 'Immune Checkpoint Blockade',
-            fieldTotals: ['Datasets: 21', 'Canonical: 21'],
-            contains: ['rnaseq', 'microarray'],
-            description:
-                "Data focused with how an individual's genetic attributes affect the likely response to therapeutic drugs.",
-            img: 'clinical_icb.png',
-            path: 'clinical_icb'
-        },
-        {
-            name: 'Radiomics',
-            fieldTotals: ['Datasets: 3', 'Canonical: 3'],
-            contains: ['CT imaging'],
-            description:
-                "Data focused on how the genome is involved in the body's response to environmental stressors and toxicants.",
-            img: 'radiomics.png',
-            path: 'radiomicset'
-        },
-        {
-            name: 'Annotations',
-            fieldTotals: ['Datasets: 1', 'Canonical: 1'],
-            contains: ['Drug Toxicity', 'BioAssays'],
-            description: '',
-            img: 'radiomics.png',
-            path: 'annotations'
-        }
-    ];
+    const [stats, setStats] = useState([]);
 
     useEffect(() => {
-        path.setDatatype('');
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        const getStats = async () => {
+            const res = await axios.get('/api/view/statistics');
+            setStats(res.data);
+            console.log(res.data);
+        };
+        getStats();
     }, []);
 
     return (
         <div className="min-h-screen flex flex-col items-center gap-10 mx-auto max-w-[1300px] bg-gray-50 py-40">
             <div className="flex flex-row flex-wrap gap-4 justify-center">
-                {data.map(type => (
-                    // Or this one<div className="group w-[300px] hover:h-[400px] flex flex-col rounded-3xl drop-shadow-md bg-white transition-all duration-700 overflow-hidden">
-                    <div
-                        // onClick={() => navigate('/radiomicset/6759d669d4ed32294de67010')}
-                        onClick={() => navigate(`/datatypes/${type.path}`)}
-                        className="group w-[380px] h-[320px] flex flex-col rounded-3xl drop-shadow-md bg-white transition-all duration-700 overflow-hidden hover:cursor-pointer"
-                    >
-                        <div className="w-full h-[150px] rounded-t-3xl flex flex-col justify-center items-center overflow-hidden">
-                            <img src={`/images/datatype-icons/${type.img}`} />
-                        </div>
-                        <div className="flex flex-col p-3">
-                            <div className="flex flex-col min-h-[170px] gap-2">
-                                <h2 className="text-headingXl font-semibold text-lightBlue">{type.name}</h2>
-
-                                <div className="flex flex-row flex-wrap gap-2">
-                                    {type.contains.map(field => (
-                                        <div key={field} className="flex flex-row bg-lightBlue px-2 py-1 rounded-lg">
-                                            <span className="text-bodySm font-bold text-white">{field}</span>
-                                        </div>
-                                    ))}
-                                </div>
-
-                                <div className="flex flex-row flex-wrap gap-2">
-                                    {type.fieldTotals.map(field => (
-                                        <div
-                                            key={field}
-                                            className="flex flex-row bg-lightYellow bg-opacity-60 px-2 py-1 rounded-lg"
-                                        >
-                                            <span className="text-bodySm text-darkBlue font-bold">{field}</span>
-                                        </div>
-                                    ))}
-                                </div>
+                {stats &&
+                    stats.map(type => (
+                        <div
+                            onClick={() => navigate(`/datatypes/${type.path}`)}
+                            className="group w-[380px] h-[320px] flex flex-col rounded-3xl drop-shadow-md bg-white transition-all duration-700 overflow-hidden hover:cursor-pointer"
+                        >
+                            <div className="w-full h-[150px] rounded-t-3xl flex flex-col justify-center items-center overflow-hidden">
+                                <img src={`/images/datatype-icons/${type.img}`} />
                             </div>
+                            <div className="flex flex-col p-3">
+                                <div className="flex flex-col min-h-[170px] gap-2">
+                                    <h2 className="text-headingXl font-semibold text-lightBlue">{type.name}</h2>
+                                    <div className="flex flex-row flex-wrap gap-2">
+                                        {type.dataSources.map(field => (
+                                            <div
+                                                key={field}
+                                                className="flex flex-row bg-lightBlue px-2 py-1 rounded-lg"
+                                            >
+                                                <span className="text-bodySm font-bold text-white">{field}</span>
+                                            </div>
+                                        ))}
+                                    </div>
 
-                            <p className="text-bodyMd text-gray-600 opacity-0 max-h-0 group-hover:opacity-100 group-hover:max-h-32 transition-all duration-700 overflow-hidden">
-                                {type.description}
-                            </p>
+                                    {type.datasetCount && type.canonicalCount && (
+                                        <div className="flex flex-row flex-wrap gap-2">
+                                            <div className="flex flex-row bg-lightYellow bg-opacity-60 px-2 py-1 rounded-lg">
+                                                <span className="text-bodySm text-darkBlue font-bold">
+                                                    Datasets: {type.datasetCount}
+                                                </span>
+                                            </div>
+                                            <div className="flex flex-row bg-lightYellow bg-opacity-60 px-2 py-1 rounded-lg">
+                                                <span className="text-bodySm text-darkBlue font-bold">
+                                                    Canonical Datasets: {type.canonicalCount}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+
+                                <p className="text-bodyMd text-gray-600 opacity-0 max-h-0 group-hover:opacity-100 group-hover:max-h-32 transition-all duration-700 overflow-hidden">
+                                    {type.description}
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
             </div>
         </div>
     );
