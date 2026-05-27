8f417452ed647253cf329fd07bd37fef753a7c7b
mattbocc <matthew.boccalon@live.ca>
2026-05-27 15:14:52 -0400

    feat: updated database models to align with fields coming from front-end

--- db/models/dataset-object.js
+++ db/models/dataset-object.js
@@ -3,19 +3,20 @@ const Schema = mongoose.Schema;
 
 const datasetObjectSchema = new Schema({
 	name: { type: String, required: true },
+	dateSubmitted: { type: Date },
 	version: { type: String, required: true },
 	datasetType: { type: String, required: true },
 	info: {
-        status: {type: String, required: true},
-        private: Boolean,
+		status: { type: String, required: true },
+		private: Boolean,
 		disabled: Boolean,
-        canonical: Boolean,
-        numDownload: Number,
-        createdBy: String,
-        shareToken: String,
-        dateCreated: Date,
-        other: Object
-    },
+		canonical: Boolean,
+		numDownload: Number,
+		createdBy: String,
+		shareToken: String,
+		dateCreated: Date,
+		other: Object
+	},
 	publications: [
 		{
 			citation: String,
@@ -25,20 +26,24 @@ const datasetObjectSchema = new Schema({
 	description: String,
 	name: { type: String, required: true },
 	datasetType: { type: String, required: true },
-    repositories: {
-		github: {
-			commitId: String,
-			url: String
-		},
-		zenodo: {
-			version: String,
+	repositories: {
+		doi: String,
+		downloadLink: [String],
+		csvLinks: [String],
+		// Should be the new way we store links instead of in info.other
+		// github: {
+		// 	commitId: String,
+		// 	url: String
+		// },
+		// zenodo: {
+		// 	version: String,
+		// 	doi: String,
+		// 	downloadLink: String
+		// },
+		bioComputeObject: {
 			doi: String,
 			downloadLink: String
 		},
-        bioComputeObject: {
-            doi: String,
-            downloadLink: String
-        },
 	},
 	releaseNotes: { type: Schema.Types.Mixed, default: {} },
 	dataSources: { type: Schema.Types.Mixed, default: {} },
@@ -49,18 +54,18 @@ const datasetObjectSchema = new Schema({
 			url: String
 		}
 	],
-	rawData: [
-		{
-			heading: String,
-			metaData: [
-				{
-					name: String,
-					description: String,
-					url: String,
-				},
-			]
-		},
-	],
+	// rawData: [
+	// 	{
+	// 		heading: String,
+	// 		metaData: [
+	// 			{
+	// 				name: String,
+	// 				description: String,
+	// 				url: String,
+	// 			},
+	// 		]
+	// 	},
+	// ],
 	datasetNote: { type: mongoose.Schema.Types.ObjectId, ref: "DatasetNote" },
 
 });
