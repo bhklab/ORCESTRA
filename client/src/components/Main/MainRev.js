de532fe14176f91b44d672489ebb75ac9615696a
mattbocc <matthew.boccalon@live.ca>
2026-05-27 15:15:29 -0400

    bug: fixed console errors thrown for wrong JSX notation

--- client/src/components/Main/MainRev.js
+++ client/src/components/Main/MainRev.js
@@ -33,10 +33,10 @@ const MainRev = () => {
                 <div className="flex flex-col gap-2 border-1 border-gray-100 p-4 rounded-md shadow-md">
                     <div className="flex flex-row gap-4 items-center">
                         <div className="bg-lightYellow p-3 rounded-lg">
-                            <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="#001D3D" stroke-width="2">
+                            <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="#001D3D" strokeWidth="2">
                                 <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                     d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
                                 ></path>
                             </svg>
@@ -51,10 +51,10 @@ const MainRev = () => {
                 <div className="flex flex-col gap-2 border-1 border-gray-100 p-4 rounded-md shadow-md">
                     <div className="flex flex-row gap-4 items-center">
                         <div className="bg-lightYellow p-3 rounded-lg">
-                            <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="#001D3D" stroke-width="2">
+                            <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="#001D3D" strokeWidth="2">
                                 <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                     d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"
                                 ></path>
                             </svg>
@@ -68,10 +68,10 @@ const MainRev = () => {
                 <div className="flex flex-col gap-2 border-1 border-gray-100 p-4 rounded-md shadow-md">
                     <div className="flex flex-row gap-4 items-center">
                         <div className="bg-lightYellow p-3 rounded-lg">
-                            <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="#001D3D" stroke-width="2">
+                            <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="#001D3D" strokeWidth="2">
                                 <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                     d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
                                     data-fg-kqz25="1.18:1.7585:/src/app/components/LandingPage.tsx:68:19:2727:248:e:path"
                                 ></path>
