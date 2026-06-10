ab74502194d5a237f6c7c1f9cb5174770fa29898
mattbocc <matthew.boccalon@live.ca>
2026-06-10 11:12:03 -0400

    feat: create run pipeline page with popup toasts for success and failure run submissions

--- client/src/components/Admin/RunPipeline.js
+++ client/src/components/Admin/RunPipeline.js
@@ -1,105 +1,211 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import CustomInputText from '../Shared/CustomInputText';
-import CustomMessages from '../Shared/CustomMessages';
-import CustomSelect from '../Shared/CustomSelect';
-import CustomCheckbox from '../Shared/CustomCheckbox';
-import { Button } from 'primereact/button';
-import styled from 'styled-components';
-import { ThreeDots } from 'react-loader-spinner';
-import * as Mainstyle from '../Main/MainStyle'
-
-const StyledRunPipeline = styled.div`
-  max-width: 800px;
-  .content {
-    display: flex;
-    margin-top: 30px;
-    margin-bottom: 20px;
-    .left {
-      width: 400px;
-      margin-right: 30px;
-    }
-    .right {
-      width: 400px;
-      background-color: #ffffff;
-      border-radius: 5px;
-      padding: 0 15px 0 15px;
-    }
-    .sub-header {
-      display: flex;
-      align-items: center;
-      h4 {
-        margin-right: 20px;
-      }
-    }
-    .field {
-      display: flex;
-      align-items: center;
-      width: 200px;
-      margin-bottom: 10px;
-      .delete-btn {
-        margin-right: 10px;
-      }
-      .textfield {
-        margin-right: 10px;
-      }
-    }
-    .paramfield {
-      margin-bottom: 10px;
-    }
-  }
-`;
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Toast } from 'primereact/toast';
 
 const RunPipeline = () => {
+    const [createdPipelines, setCreatedPipelines] = useState([]);
+    const [selectedCreatePipeline, setSelectedCreatePipeline] = useState({});
+    const [runPipelineFields, setRunPipelineFields] = useState({
+        commit_id: '',
+        branch: '',
+        email: '',
+        output_directories: [],
+        snakefile_path: '',
+        config_file_path: '',
+        conda_env_file_path: '',
+        pixi_use: false,
+        larger_machine_use: false,
+        pipeline_run_command: '',
+        qc_command: '',
+        new_release: false
+    });
 
-  const [pipelines, setPipelines] = useState([]);
-  const [selected, setSelected] = useState(null);
-  const [selectedPipeline, setSelectedPipeline] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [showMsg, setShowMsg] = useState(false);
-  const [submitMessage, setSubmitMessage] = useState({});
+    const toast = useRef(null);
 
+    useEffect(() => {
+        const getCreatedPipelines = async () => {
+            const res = await axios.get('/api/user/created_pipelines');
+            setCreatedPipelines(res.data);
+        };
+        getCreatedPipelines();
+    }, []);
 
-  return(
-    <StyledRunPipeline>
-      <CustomMessages trigger={showMsg} message={submitMessage} />
-      <h3>Run Pipeline</h3>
-      {
-        // pipelines.length > 0 &&  
-        <div>
-          <CustomSelect 
-            selectOne
-            selected={selected}
-            options={pipelines.map(pipeline => ({label: pipeline.name, value: pipeline.name}))}
-            label='Select pipeline: '
-          />
-          {
-            // selectedPipeline &&
-            <React.Fragment>
-              <div className='content'>
-                
-                  <CustomCheckbox 
-                    label='Run all:' 
-                  />
+    useEffect(() => {
+        console.log(runPipelineFields);
+    }, [runPipelineFields]);
+
+    const submitRunPipeline = async () => {
+        setRunPipelineFields(...runPipelineFields, selectedCreatePipeline.pipeline_name);
+        try {
+            const res = await axios.post('/api/user/run_pipeline', runPipelineFields);
+            toast.current.show({
+                severity: 'success',
+                summary: 'Success',
+                detail: `Successful pipeline submission: ${res.data}`,
+                life: 3000
+            });
+        } catch (error) {
+            toast.current.show({
+                severity: 'error',
+                summary: 'Error',
+                detail: `Failed to submit pipeline: ${error}`,
+                life: 3000
+            });
+        }
+    };
+
+    return (
+        <div className="flex flex-col gap-2">
+            <Toast ref={toast} />
+            <DataTable
+                value={createdPipelines}
+                sortOrder={-1}
+                size="small"
+                showGridlines={true}
+                stripedRows
+                selectionMode="single"
+                selection={selectedCreatePipeline}
+                onSelectionChange={e => setSelectedCreatePipeline(e.value)}
+            >
+                <Column field="pipeline_name" header="Pipeline Name"></Column>
+                <Column field="git_url" header="Github URL"></Column>
+            </DataTable>
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">Pipeline Name</label>
+                    <input
+                        type="text"
+                        name="pipeline_name"
+                        disabled={true}
+                        value={selectedCreatePipeline.pipeline_name}
+                        className="px-2 py-1 rounded-md text-bodyLg max-w-96 focus:outline-none bg-gray-200"
+                    />
                 </div>
-              
-              {
-                loading ?
-                <ThreeDots color="#3D405A" height={100} width={100} />
-                :
-                <Mainstyle.Button 
-                  className='p-button-primary'
-                  label='Run Pipeline'
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">Github URL</label>
+                    <input
+                        type="text"
+                        name="git_url"
+                        disabled={true}
+                        value={selectedCreatePipeline.git_url}
+                        className="px-2 py-1 rounded-md text-bodyLg max-w-96 focus:outline-none bg-gray-200"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">Commit SHA</label>
+                    <input
+                        type="text"
+                        name="commit_id"
+                        value={runPipelineFields.commit_id}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">Branch</label>
+                    <input
+                        type="text"
+                        name="branch"
+                        value={runPipelineFields.branch}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">Email</label>
+                    <input
+                        type="text"
+                        name="email"
+                        value={runPipelineFields.email}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">Snakefile Path</label>
+                    <input
+                        type="text"
+                        name="snakefile_path"
+                        value={runPipelineFields.snakefile_path}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">Configuration File Path</label>
+                    <input
+                        type="text"
+                        name="config_file_path"
+                        value={runPipelineFields.config_file_path}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">Conda Environment File Path</label>
+                    <input
+                        type="text"
+                        name="conda_env_file_path"
+                        value={runPipelineFields.conda_env_file_path}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                    <label className="text-bodyMd font-semibold text-gray-600">Pixi Use</label>
+                    <input
+                        type="checkbox"
+                        name="pixi_use"
+                        checked={runPipelineFields.pixi_use}
+                        onChange={e => setRunPipelineFields({ ...runPipelineFields, pixi_use: e.target.checked })}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                    <label className="text-bodyMd font-semibold text-gray-600">Larger Machine Use</label>
+                    <input
+                        type="checkbox"
+                        name="larger_machine_use"
+                        checked={runPipelineFields.larger_machine_use}
+                        onChange={e =>
+                            setRunPipelineFields({ ...runPipelineFields, larger_machine_use: e.target.checked })
+                        }
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">Pipeline Run Command</label>
+                    <input
+                        type="text"
+                        name="pipeline_run_command"
+                        value={runPipelineFields.pipeline_run_command}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="text-bodyMd font-semibold text-gray-600">QC Command</label>
+                    <input
+                        type="text"
+                        name="qc_command"
+                        value={runPipelineFields.qc_command}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                    <label className="text-bodyMd font-semibold text-gray-600">New Release</label>
+                    <input
+                        type="checkbox"
+                        name="new_release"
+                        checked={runPipelineFields.new_release}
+                        onChange={e => setRunPipelineFields({ ...runPipelineFields, new_release: e.target.checked })}
+                        className="px-2 py-1 rounded-md border-1 border-darkYellow text-bodyLg max-w-96 focus:outline-none"
+                    />
+                </div>
+                <button
+                    className="bg-darkBlue text-white px-2 py-2 rounded-md text-bodyMd font-semibold max-w-24"
+                    // onClick={e => ()}
                 >
-					Run Pipeline
-				</Mainstyle.Button>
-              }
-            </React.Fragment>
-          }
+                    Submit
+                </button>
+            </div>
         </div>
-      }
-    </StyledRunPipeline>
-  );
-}
+    );
+};
 
 export default RunPipeline;
